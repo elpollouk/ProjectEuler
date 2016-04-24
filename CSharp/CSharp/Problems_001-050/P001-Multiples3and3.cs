08e615639e500f4dbbe84477fe0ef963b78daaa1
Adrian O'Grady <elpollouk@gmail.com>
2016-04-24 20:01:03 +0100

    Solution 001: Multiples of 3 and 5

---
+++ CSharp/CSharp/Problems_001-050/P001-Multiples3and3.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Xunit;
+
+// Multiples of 3 and 5
+// https://projecteuler.net/problem=1
+namespace CSharp.Problems_001_050
+{
+    public class P001_Multiples3and3
+    {
+        int Total(int range)
+        {
+            var total = 0;
+            while (--range > 0)
+            {
+                if ((range % 3) == 0 || (range % 5) == 0)
+                {
+                    total += range;
+                }
+            }
+
+            return total;
+        }
+
+        [Fact]
+        public void Example() => Total(10).Should().Be(23);
+
+        [Fact]
+        public void Solution() => Total(1000).Should().Be(233168);
+    }
+}
