f2993c7cade5e8159624c1a2e6ca64e336c2dd4e
Adrian O'Grady <elpollouk@gmail.com>
2016-04-24 20:30:18 +0100

    Solution 002: Even Fibonacci numbers

---
+++ CSharp/CSharp/Problems_001-050/P002-EvenFibonacci.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Xunit;
+
+// Even Fibonacci numbers
+// https://projecteuler.net/problem=2
+namespace CSharp.Problems_001_050
+{
+    public class P002_EvenFibonacci
+    {
+        int Total(int valueLimit)
+        {
+            var selection = false;
+            var a = 1;
+            var b = 1;
+            var sum = 0;
+
+            while (a < valueLimit && b < valueLimit)
+            {
+                var current = a + b;
+                if (current % 2 == 0)
+                    sum += current;
+
+                if (selection = !selection)
+                    a = current;
+                else
+                    b = current;
+            }
+
+            return sum;
+        }
+
+        [Fact]
+        public void Example() => Total(89).Should().Be(44);
+
+        [Fact]
+        public void Solution() => Total(4000000).Should().Be(4613732);
+    }
+}
