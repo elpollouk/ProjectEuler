ba1cab99f7b7dc2464579996c0910f9a5b438b66
Adrian O'Grady <adriano@microsoft.com>
2016-04-25 13:15:50 +0100

    Solution 003: Largest Prime Factor

---
+++ CSharp/CSharp/Problems_001-050/P003-LargestPrimeFactor.cs
@@ -0,0 +1,88 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CSharp.Problems_001_050
+{
+    public class P003_LargestPrimeFactor
+    {
+        // Store all the primes we've found so far so we're not always recalulating the same values (dynamic programming I guess?)
+        HashSet<long> primes = new HashSet<long>();
+        long highestPrime = 2;
+
+        bool IsPrime(long value)
+        {
+            if (value == 1 || value == 2) return true;
+            if (primes.Contains(value)) return true;
+            if (value < highestPrime) return false; // We know we've already checked all the values below highest prime and if it's not in the set, then it's not a prime
+
+            for (var i = 2; i * i <= value; i++)
+            {
+                if (value % i == 0) return false;
+            }
+
+            primes.Add(value);
+            highestPrime = value;
+
+            return true;
+        }
+
+        long LargestPrimeFactor(long value)
+        {
+            while (!IsPrime(value))
+            {
+                // Special case for 2
+                if (value % 2 == 0) value /= 2;
+                else for (int i = 3; i < (value / 2); i += 2) // We know that no other even number is going to be a prime factor, so why even bother checking them?
+                {
+                    if (IsPrime(i) && value % i == 0)
+                    {
+                        value /= i;
+                        break;
+                    }
+                }
+            }
+
+            return value;
+        }
+
+        [Theory]
+        [InlineData(1, true)]
+        [InlineData(2, true)]
+        [InlineData(3, true)]
+        [InlineData(4, false)]
+        [InlineData(5, true)]
+        [InlineData(6, false)]
+        [InlineData(7, true)]
+        [InlineData(8, false)]
+        [InlineData(9, false)]
+        [InlineData(10, false)]
+        [InlineData(11, true)]
+        [InlineData(12, false)]
+        [InlineData(13, true)]
+        [InlineData(14, false)]
+        [InlineData(15, false)]
+        [InlineData(23, true)]
+        public void Primes(long value, bool expected) => IsPrime(value).Should().Be(expected);
+
+        [Fact]
+        public void Example() => LargestPrimeFactor(13195).Should().Be(29);
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 3)]
+        [InlineData(4, 2)]
+        [InlineData(5, 5)]
+        [InlineData(6, 3)]
+        [InlineData(7, 7)]
+        [InlineData(8, 2)]
+        [InlineData(9, 3)]
+        [InlineData(10, 5)]
+        [InlineData(23, 23)]
+        public void Check(long value, long largestPrime) => LargestPrimeFactor(value).Should().Be(largestPrime);
+
+        [Fact]
+        public void Solution() => LargestPrimeFactor(600851475143).Should().Be(6857);
+    }
+}
