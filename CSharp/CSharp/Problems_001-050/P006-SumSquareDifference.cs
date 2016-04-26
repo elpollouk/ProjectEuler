5a731d68c2999ef4ffba10809b6e58cdd0a24cd9
Adrian O'Grady <adriano@microsoft.com>
2016-04-26 13:31:08 +0100

    Solution 006: Sum Square Difference

---
+++ CSharp/CSharp/Problems_001-050/P006-SumSquareDifference.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CSharp.Problems_001_050
+{
+    public class P006_SumSquareDifference
+    {
+        int Calc(int range)
+        {
+            var sumRange = range * (range + 1);
+            sumRange /= 2;
+
+            var sumSquare = range * (2 * range + 1) * (range + 1);
+            sumSquare /= 6;
+
+            return (sumRange * sumRange) - sumSquare;
+        }
+
+        [Fact]
+        public void Example() => Calc(10).Should().Be(2640);
+
+        [Fact]
+        public void Solution() => Calc(100).Should().Be(25164150);
+    }
+}
