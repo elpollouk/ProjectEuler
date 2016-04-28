04522a33cb897624040a6a9990aad8055695e320
Adrian O'Grady <adriano@microsoft.com>
2016-04-28 13:39:42 +0100

    Solution 009: Special Pythagorean Triplet

---
+++ CSharp/CSharp/Problems_001-050/P009-SpecialPythagoreanTriplet.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CSharp.Problems_001_050
+{
+    public class P009_SpecialPythagoreanTriplet
+    {
+        int TripletProduct(int requiredSum)
+        {
+            for (var i = 1; i < requiredSum; i++)
+            {
+                for (var j = 1; j < requiredSum; j++)
+                {
+                    if (i + j < requiredSum / 2) continue;
+                    var target = requiredSum - (i + j);
+                    if (target < 0) break;
+
+                    if (i * i + j * j == target * target)
+                        return i * j * target;
+                }
+            }
+
+            return 0;
+        }
+
+        [Fact]
+        public void Example() => TripletProduct(12).Should().Be(60);
+
+        [Fact]
+        public void Solution() => TripletProduct(1000).Should().Be(31875000);
+    }
+}
