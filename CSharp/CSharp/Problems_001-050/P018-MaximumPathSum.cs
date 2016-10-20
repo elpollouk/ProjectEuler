35055183638bef73edb79c038df1b2f1bce514e4
Adrian O'Grady <elpollouk@gmail.com>
2016-10-21 00:40:10 +0100

    Solution 018: Maximum Path Sum I

---
+++ CSharp/CSharp/Problems_001-050/P018-MaximumPathSum.cs
@@ -0,0 +1,69 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CSharp.Problems_001_050
+{
+    public class P018_MaximumPathSum
+    {
+        private int[] Row(params int[] row) => row;
+
+        private static long MaxPathSum(params int[][] rows)
+        {
+            var max = 0;
+            for (var row = 1; row < rows.Length; row++)
+            {
+                var previousRow = rows[row - 1];
+                var currentRow = rows[row];
+
+                // Edge values
+                currentRow[0] += previousRow[0];
+                currentRow[row] += previousRow[row - 1];
+
+                max = (currentRow[0] < currentRow[row]) ? currentRow[row] : currentRow[0];
+
+                // Inner values
+                for (var i = 1; i < currentRow.Length - 1; i++)
+                {
+                    var maxFeeder = (previousRow[i - 1] < previousRow[i]) ? previousRow[i] : previousRow[i - 1];
+                    currentRow[i] += maxFeeder;
+                    if (max < currentRow[i]) max = currentRow[i];
+                }
+            }
+
+            return max;
+        }
+
+        [Fact]
+        public void Example()
+        {
+            MaxPathSum(
+                Row(3),
+                Row(7, 4),
+                Row(2, 4, 6),
+                Row(8, 5, 9, 3)
+            ).Should().Be(23);
+        }
+
+        [Fact]
+        public void Solution()
+        {
+            MaxPathSum(
+                Row(75),
+                Row(95, 64),
+                Row(17, 47, 82),
+                Row(18, 35, 87, 10),
+                Row(20,  4, 82, 47, 65),
+                Row(19,  1, 23, 75,  3, 34),
+                Row(88,  2, 77, 73,  7, 63, 67),
+                Row(99, 65,  4, 28,  6, 16, 70, 92),
+                Row(41, 41, 26, 56, 83, 40, 80, 70, 33),
+                Row(41, 48, 72, 33, 47, 32, 37, 16, 94, 29),
+                Row(53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14),
+                Row(70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57),
+                Row(91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48),
+                Row(63, 66,  4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31),
+                Row( 4, 62, 98, 27, 23,  9, 70, 98, 73, 93, 38, 53, 60,  4, 23)
+            ).Should().Be(1074);
+        }
+    }
+}
